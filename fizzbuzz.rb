0feadc462b00a16a87eb479fd5a31455452d1eb7
pikka <j1415@matsue-ct.jp>
2017-11-21 10:04:12 +0900

    complete fizzbuz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 100.times{|i|
 	if i&3 == 0 && i%5 == 0 then
-		p FizzBuzz
-	elseif i%3 == 0 then
-		p Fizz
-	elseif i%5 == 0 then
-		p Buzz
+		p "FizzBuzz"
+	elsif i%3 == 0 then
+		p "Fizz"
+	elsif i%5 == 0 then
+		p "Buzz"
 	else
 		p i
 	end
+}
